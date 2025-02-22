0b5022d83ec314ab7614c39813c665f6ee200f30
dduart3 <57542832+dduart3@users.noreply.github.com>
2025-02-22 16:07:25 -0400

    set initial state to false

--- src/modules/product-viewer/state/viewer.ts
+++ src/modules/product-viewer/state/viewer.ts
@@ -2,7 +2,7 @@ import { atom } from "jotai";
 import { ViewerState } from "../../../types/Product";
 
 export const viewerStateAtom = atom<ViewerState>({
-  isOpen: true, // Start with the viewer closed
+  isOpen: false, // Start with the viewer closed
   currentProduct: null,
   catalog: [], // Populate this with your product data
   currentIndex: 0,
