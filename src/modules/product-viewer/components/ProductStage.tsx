13d8fa6d6e60d348ca07747be985101606e131cb
dduart3 <57542832+dduart3@users.noreply.github.com>
2025-02-22 16:07:16 -0400

    modified product stage to simplify it

--- src/modules/product-viewer/components/ProductStage.tsx
+++ src/modules/product-viewer/components/ProductStage.tsx
@@ -1,28 +1,33 @@
+import { useAtom } from "jotai";
+import { viewerStateAtom } from "../state/viewer";
+import { Model } from "../../../components/Model";
+import { GroupProps, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { Group } from "three";
 
-import { useAtom } from 'jotai'
-import { viewerStateAtom } from '../state/viewer'
-import { Model } from '../../../components/Model'
-import { useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
-import { Group } from 'three'
+export const ProductStage = ({
+  isDragging,
+  rotationSpeed,
+  ...props
+}: {
+  isDragging: boolean;
+  rotationSpeed: number;
+} & GroupProps) => {
+  //const [viewerState] = useAtom(viewerStateAtom);
+  const groupRef = useRef<Group>(null);
 
-export const ProductStage = () => {
-  const [viewerState] = useAtom(viewerStateAtom)
-  const groupRef = useRef<Group>(null)
-  
   useFrame(() => {
-    if (groupRef.current) {
-      groupRef.current.rotation.y += 0.01
+    if (groupRef.current === null) return;
+    if (isDragging) {
+      groupRef.current.rotation.y += rotationSpeed;
+    } else {
+      groupRef.current.rotation.y += 0.01;
     }
-  })
-  
-  
+  });
+
   return (
-    <group ref={groupRef} position={[0, 0, -3]} scale={[0.5, 0.5, 0.5]}>
-      <Model 
-        modelPath={'/products/men/shoes/oxford'}
-        position={[0, -0.5, 0]}
-      />
+    <group ref={groupRef} scale={8} {...props}>
+      <Model modelPath={"/products/men/shoes/oxford"} />
     </group>
-  )
-}
+  );
+};
