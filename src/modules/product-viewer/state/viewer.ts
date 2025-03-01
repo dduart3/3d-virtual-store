30aac82f237e51318795caf42e477ca37ab213d6
dduart3 <57542832+dduart3@users.noreply.github.com>
2025-03-01 08:21:39 -0400

    .

--- src/modules/product-viewer/state/viewer.ts
+++ src/modules/product-viewer/state/viewer.ts
@@ -2,7 +2,7 @@ import { atom } from "jotai";
 import { ViewerState } from "../types/viewer";
 
 export const viewerStateAtom = atom<ViewerState>({
-  isOpen: true,
+  isOpen: false,
   currentProduct: null,
   catalog: null,
   currentIndex: 0,
