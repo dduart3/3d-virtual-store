77dec38f5d5b0201b0e9fcdd3333fd61cbd4f7b2
dduart3 <57542832+dduart3@users.noreply.github.com>
2025-03-02 16:19:20 -0400

    removed dev mode code

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,42 +1,15 @@
 import { Environment } from "@react-three/drei";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { Fade, FadeHandle } from "./shared/components/Fade";
 import { Avatar } from "./modules/avatar/components/Avatar";
 import { ViewerScene } from "./modules/product-viewer/components/ViewerScene";
-import { useAtom, useSetAtom } from "jotai";
+import { useAtom } from "jotai";
 import { viewerStateAtom } from "./modules/product-viewer/state/viewer";
 import { StoreScene } from "./modules/store/components/StoreScene";
-import { fadeRefAtom } from "./shared/state/fade";
-import { devModeAtom } from "./shared/state/dev";
 
 export const Experience = () => {
   const fadeRef = useRef<FadeHandle>(null!)
-  const [viewerState, setViewerState] = useAtom(viewerStateAtom);
-  const setFadeRef = useSetAtom(fadeRefAtom);
-  const  [isDevMode, setDevMode] = useAtom(devModeAtom);
-
-  const toggleViewer = () => {
-    fadeRef.current.fadeToBlack();
-    setTimeout(() => {
-      setViewerState(prev => ({ ...prev, isOpen: !prev.isOpen }));
-      fadeRef.current.fadeFromBlack();
-    }, 1000);
-  }
-
-  const toggleDevMode = () => {
-    setDevMode(prev => !prev);
-  }
-
-  useEffect(() => {
-    const button = document.getElementById("viewer");
-    const button2 = document.getElementById("dev");
-
-    if (button && button2) {
-      button.onclick = toggleViewer;
-      button2.onclick = toggleDevMode;
-    }
-    setFadeRef(fadeRef.current);
-  }, [])
+  const [viewerState] = useAtom(viewerStateAtom);
 
   return (
     <group>
