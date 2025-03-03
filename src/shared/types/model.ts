a2f164556634bffcc2ee775002d36463cd02b390
dduart3 <57542832+dduart3@users.noreply.github.com>
2025-03-03 09:49:37 -0400

    removed duplicated types

--- src/shared/types/model.ts
+++
@@ -1,9 +0,0 @@
-import { Euler, Vector3 } from "three";
-
-export type Model = {
-  path: string;
-  position: [number, number, number] | Vector3;
-  rotation?: [number, number, number] | Euler;
-  scale?: number | [number, number, number];
-  label?: string;
-};
