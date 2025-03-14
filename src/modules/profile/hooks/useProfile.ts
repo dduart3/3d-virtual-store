5cb8cddd53a29fef1647eefb29efdd0a4a2c03ee
dduart3 <57542832+dduart3@users.noreply.github.com>
2025-03-14 00:39:59 -0400

    .

--- src/modules/profile/hooks/useProfile.ts
+++ src/modules/profile/hooks/useProfile.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation} from '@tanstack/react-query';
 import { supabase } from '../../../lib/supabase';
 import { queryClient } from '../../../lib/queryClient';
 
